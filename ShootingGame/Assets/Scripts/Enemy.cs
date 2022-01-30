6d78c4b108d80cce44b9cac375ce5ccf41c724ad
94mark <90877724+94mark@users.noreply.github.com>
2022-01-30 18:08:33 +0900

    Update Enemy.cs
    30% 확률로 플레이어 방향 / 나머지 확률로 아래 방향 설정

--- ShootingGame/Assets/Scripts/Enemy.cs
+++ ShootingGame/Assets/Scripts/Enemy.cs
@@ -6,11 +6,34 @@ public class Enemy : MonoBehaviour
 {
     //필요 속성 : 이동 속도
     public float speed = 5;
+    //방향을 전역 변수로 만들어 Start와 Update에서 사용
+    Vector3 dir;
 
+    void Start()
+    {
+        //Vector3 dir;
+        //0부터 9까지 10개의 값 중에 하나를 랜덤으로 가져온다
+        int randValue = Random.Range(0, 10);
+        //만약 3보다 작으면 플레이어 방향
+        if(randValue < 3)
+        {
+            //플레이어를 찾아 target으로 한다
+            GameObject target = GameObject.Find("Player");
+            //방향을 구하고 싶다. target - me
+            dir = target.transform.position - transform.position;
+            //방향의 크기를 1로 한다(방향 벡터 설정)
+            dir.Normalize();
+        }
+        //그렇지 않으면 아래 방향으로 정한다
+        else
+        {
+            dir = Vector3.down;
+        }
+    }
     void Update()
     {
         //1. 방향을 구한다
-        Vector3 dir = Vector3.down;
+        //Vector3 dir = Vector3.down;
         //2. 이동
         transform.position += dir * speed * Time.deltaTime;
     }

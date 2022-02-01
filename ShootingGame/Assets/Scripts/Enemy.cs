6c6fa716f9d69f8cee3b265b0e7fb82b1fa95915
94mark <90877724+94mark@users.noreply.github.com>
2022-02-01 20:06:46 +0900

    Update Enemy.cs
    오브젝트 풀에 총알 삽입

--- ShootingGame/Assets/Scripts/Enemy.cs
+++ ShootingGame/Assets/Scripts/Enemy.cs
@@ -64,6 +64,11 @@ public class Enemy : MonoBehaviour
         {
             //2. 부딪힌 물체를 비활성화
             other.gameObject.SetActive(false);
+
+            //PlayerFire 클래스 얻어오기
+            PlayerFire player = GameObject.Find("Player").GetComponent<PlayerFire>();
+            //리스트에 총알 삽입
+            player.bulletObjectPool.Add(other.gameObject);
         }
         else
         {

ff11a0eba6e0edede9001ad9a70cf766ba1c6a22
94mark <90877724+94mark@users.noreply.github.com>
2022-02-01 19:15:43 +0900

    Update EnemyManager.cs
    에너미 오브젝트 풀 활용

--- ShootingGame/Assets/Scripts/EnemyManager.cs
+++ ShootingGame/Assets/Scripts/EnemyManager.cs
@@ -10,28 +10,53 @@ public class EnemyManager : MonoBehaviour
     float minTime = 1; //최소 시간
     float maxTime = 5; //최대 시간
 
+    public int poolSize = 10; //오브젝트 풀 크기
+    GameObject[] enemyObjectPool; //오브젝트 풀 배열
+    public Transform[] spawnPoints; //SpawnPoints들
+
     void Start()
     {
         //태어날 때 적의 생성 시간을 설정
         createTime = Random.Range(minTime, maxTime);
+
+        //오브젝트 풀을 에너미들을 담을 수 있는 크기로 만들어준다
+        enemyObjectPool = new GameObject[poolSize];
+        //오브젝트 풀에 넣을 에너미 개수만큼 반복
+        for (int i = 0; i < poolSize; i++)
+        {
+            //에너미 공장에서 에너미 생성
+            GameObject enemy = Instantiate(enemyFactory);
+            //에너미를 오브젝트 풀에 넣는다
+            enemyObjectPool[i] = enemy;
+            //비활성화
+            enemy.SetActive(false);
+        }
     }
 
     void Update()
     {
-        //1.시간이 흐르다가
         currentTime += Time.deltaTime;
-
-        //2. 만약 현재 시간이 일정 시간이 되면
+        //1. 생성 시간이 되었으니까
         if(currentTime > createTime)
         {
-            //3. 적 공장에서 적을 생성해
-            GameObject enemy = Instantiate(enemyFactory);
-            //내 위치에 갖다 놓고 싶다
-            enemy.transform.position = transform.position;
-            //현재 시간을 0으로 초기화
-            currentTime = 0;
-            //적을 생성한 후 적의 생성 시간을 다시 설정
-            createTime = Random.Range(minTime, maxTime);
+            //2. 에너미 풀 안에 있는 에너미 중에서
+            for(int i = 0; i < poolSize; i++)
+            {
+                //3. 비활성화 된 에너미를
+                //만약 에너미가 비활성화 되었다면
+                GameObject enemy = enemyObjectPool[i];
+                if(enemy.activeSelf == false)
+                {
+                    //에너미 위치시키기
+                    enemy.transform.position = transform.position;
+                    //4. 에너미를 활성화하고 싶다
+                    enemy.SetActive(true);
+                    //에너미 활성화 하였기 때문에 검색 중단
+                    break;
+                }
+            }
+            createTime = Random.Range(1.0f, 5.0f);
+            createTime = 0;
         }
     }
 }

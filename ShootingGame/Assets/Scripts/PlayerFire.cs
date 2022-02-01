d2ce63f870cc595472b3d3f89639ef052b969e43
94mark <90877724+94mark@users.noreply.github.com>
2022-02-01 19:59:47 +0900

    Update PlayerFire.cs / 배열을 리스트로 수정
    배열 : 오브젝트 풀을 전수조사해 비활성화 객체를 찾는다
    
    리스트 : 비활성화 객체만 들어있기 때문에 검색이 필요없다

--- ShootingGame/Assets/Scripts/PlayerFire.cs
+++ ShootingGame/Assets/Scripts/PlayerFire.cs
@@ -7,21 +7,26 @@ public class PlayerFire : MonoBehaviour
     public GameObject bulletFactory; //총알을 생산할 공장
     
     public int poolSize = 10; //탄창에 넣을 수 있는 총알의 개수
-    GameObject[] bulletObjectPool; //오브젝트 풀 배열
+    //GameObject[] bulletObjectPool; //오브젝트 풀 배열
+    public List<GameObject> bulletObjectPool; //오브젝트 풀 배열
 
     //태어날 대 오브젝트 풀(탄창)에 총알을 하나씩 생성
     //1. 태어날 때
     void Start()
     {
         //2. 탄창을 총알 담을 수 있는 크기로 만든다
-        bulletObjectPool = new GameObject[poolSize];
+        //bulletObjectPool = new GameObject[poolSize];
+        bulletObjectPool = new List<GameObject>();
+
         //3. 탄창에 넣을 총알 개수만큼 반복
         for(int i = 0; i < poolSize; i++)
         {
             //4. 총알 공장에서 총알 생성
             GameObject bullet = Instantiate(bulletFactory);
             //5. 총알을 오브젝트 풀에 넣는다
-            bulletObjectPool[i] = bullet;
+            //bulletObjectPool[i] = bullet;
+            bulletObjectPool.Add(bullet);
+
             //비활성화
             bullet.SetActive(false);
         }
@@ -33,6 +38,21 @@ public class PlayerFire : MonoBehaviour
         //1. 발사 버튼 입력
         if(Input.GetButtonDown("Fire1"))
         {
+            //2. 탄창 안에 총알이 있다면
+            if(bulletObjectPool.Count > 0)
+            {
+                //3. 비활성화된 총알을 하나 가져온다
+                GameObject bullet = bulletObjectPool[0];
+                //4. 총알을 발사하고 싶다(활성화)
+                bullet.SetActive(true);
+                //오브젝트 풀에서 총알 제거
+                bulletObjectPool.Remove(bullet);
+
+                //총알을 위치시킨다
+                bullet.transform.position = transform.position;
+            }
+
+            /* 오브젝트 풀 배열 사용 시 
             //2. 탄창 안에 있는 총알들 중에서
             for(int i = 0; i < poolSize; i++)
             {
@@ -48,7 +68,7 @@ public class PlayerFire : MonoBehaviour
                     //총알을 발사했기 때문에 비활성화 총알 검색 중단
                     break;
                 }
-            }
+            } */
             //GameObject bullet = Instantiate(bulletFactory);
 
             //3. 총알을 발사한다(총알을 총구 위치로 가져다놓음)

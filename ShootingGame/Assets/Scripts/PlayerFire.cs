3aa18b9dd3478ca03813909c46a832470e00bff0
94mark <90877724+94mark@users.noreply.github.com>
2022-02-01 15:59:07 +0900

    Update.PlayerFire.cs
    오브젝트 풀을 이용한 총알 발사 구현

--- ShootingGame/Assets/Scripts/PlayerFire.cs
+++ ShootingGame/Assets/Scripts/PlayerFire.cs
@@ -6,24 +6,53 @@ public class PlayerFire : MonoBehaviour
 {
     public GameObject bulletFactory; //총알을 생산할 공장
     public GameObject firePosition; //총구
+    public int poolSize = 10; //탄창에 넣을 수 있는 총알의 개수
+    GameObject[] bulletObjectPool; //오브젝트 풀 배열
 
+    //태어날 대 오브젝트 풀(탄창)에 총알을 하나씩 생성
+    //1. 태어날 때
     void Start()
     {
-        
+        //2. 탄창을 총알 담을 수 있는 크기로 만든다
+        bulletObjectPool = new GameObject[poolSize];
+        //3. 탄창에 넣을 총알 개수만큼 반복
+        for(int i = 0; i < poolSize; i++)
+        {
+            //4. 총알 공장에서 총알 생성
+            GameObject bullet = Instantiate(bulletFactory);
+            //5. 총알을 오브젝트 풀에 넣는다
+            bulletObjectPool[i] = bullet;
+            //비활성화
+            bullet.SetActive(false);
+        }
     }
 
+    //목표 : 발사 버튼을 누르면 탄창에 있는 총알 중 비활성화된 것을 발사
     void Update()
     {
-        //목표 : 사용자가 발사 버튼을 누르면 총알을 발사하고 싶다
-        //속도 : 1. 사용자가 발사 버튼을 누르면
-        //만약 사용자가 발사 버튼을 누르면
+        //1. 발사 버튼 입력
         if(Input.GetButtonDown("Fire1"))
         {
-            //2. 총알 공장에서 총알을 만든다
-            GameObject bullet = Instantiate(bulletFactory);
+            //2. 탄창 안에 있는 총알들 중에서
+            for(int i = 0; i < poolSize; i++)
+            {
+                //3. 비활성화된 총알을
+                //만약 총알이 비활성화됐다면
+                GameObject bullet = bulletObjectPool[i];
+                if(bullet.activeSelf == false)
+                {
+                    //4. 총알을 발사(활성화)
+                    bullet.SetActive(true);
+                    //총알 위치
+                    bullet.transform.position = firePosition. transform.position;
+                    //총알을 발사했기 때문에 비활성화 총알 검색 중단
+                    break;
+                }
+            }
+            //GameObject bullet = Instantiate(bulletFactory);
 
             //3. 총알을 발사한다(총알을 총구 위치로 가져다놓음)
-            bullet.transform.position = firePosition.transform.position;
+            //bullet.transform.position = firePosition.transform.position;
         }
     }
 }
